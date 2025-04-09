55a38f4bee747bde19fe3b56db4a2926d09889ed
bensc <ben.schiewe@gmail.com>
2025-04-08 21:03:37 -0500

    add batch build and test

---
+++ build.bat
@@ -0,0 +1,3 @@
+java -jar ".\antlr-4.13.2-complete.jar" .\P03Schiewe.g4
+
+javac -cp ".\antlr-4.13.2-complete.jar" *.java
---
+++ test.bat
@@ -0,0 +1,41 @@
+@echo off
+echo.
+echo.
+echo.
+echo.
+echo.
+
+java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample1.txt
+echo.
+echo.
+echo.
+echo.
+echo.
+
+java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample2.txt
+echo.
+echo.
+echo.
+echo.
+echo.
+
+java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample3.txt
+echo.
+echo.
+echo.
+echo.
+echo.
+
+java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample4.txt
+echo.
+echo.
+echo.
+echo.
+echo.
+
+java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample5.txt
+echo.
+echo.
+echo.
+echo.
+echo.
