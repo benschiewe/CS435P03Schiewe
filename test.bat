1184fb973a15559a12e4acd98d73d8ebd65b9055
bensc <ben.schiewe@gmail.com>
2025-04-13 20:49:00 -0500

    more fixing tests

--- test.bat
+++ test.bat
@@ -7,35 +7,35 @@ echo.
 
 del *-output.txt
 
-java CS435P03Schiewe sample1.txt
+java CS435P03Schiewe tests/sample1.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java CS435P03Schiewe sample2.txt
+java CS435P03Schiewe tests/sample2.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java CS435P03Schiewe sample3.txt
+java CS435P03Schiewe tests/sample3.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java CS435P03Schiewe sample4.txt
+java CS435P03Schiewe tests/sample4.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java CS435P03Schiewe sample5.txt
+java CS435P03Schiewe tests/sample5.txt
 echo.
 echo.
 echo.
