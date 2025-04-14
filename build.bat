e5c7bf8e7a19da33dbe6d2d9b13fd39b30dbf209
bensc <ben.schiewe@gmail.com>
2025-04-13 20:39:39 -0500

    update test scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
-java -jar ".\antlr-4.13.2-complete.jar" .\P03Schiewe.g4
+del *.class
+del *.tokens
+del *.interp
+del P03SchieweBaseListener.java
+del P03SchieweLexer.java
+del P03SchieweListener.java
+del P03SchieweParser.java
 
-javac -cp ".\antlr-4.13.2-complete.jar" *.java
+antlr4 P03Schiewe.g4
+
+javac *.java
--- test.bat
+++ test.bat
@@ -5,35 +5,35 @@ echo.
 echo.
 echo.
 
-java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample1.txt
+java CS435P03Schiewe sample1.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample2.txt
+java CS435P03Schiewe sample2.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample3.txt
+java CS435P03Schiewe sample3.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample4.txt
+java CS435P03Schiewe sample4.txt
 echo.
 echo.
 echo.
 echo.
 echo.
 
-java -cp ".;antlr-4.13.2-complete.jar" CS435P03Schiewe sample5.txt
+java CS435P03Schiewe sample5.txt
 echo.
 echo.
 echo.
